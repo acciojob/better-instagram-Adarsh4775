c231de9ba38f6234c6a5929f40a87a6317ba2e34
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-11 15:24:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
 //your code here
+let dragged;
+
+
+function drag(event){
+	 dragged = event.target;
+}
+
+function allowDrop(event){
+	event.preventDefault();
+}
+
+function drop(event)
+{
+	event.preventDefault();
+	if(event.target.classList.contains("div"))
+	{
+		let temp=event.target.style.backgroundImage;
+
+		event.target.style.backgroundImage = dragged.style.backgroundImage;
+
+		dragged.style.backgroundImage = temp;
+	}
+}
+
+let divs = document.querySelectorAll(".div");
+divs.forEach(div=>{
+	        div.addEventListener("dragstart", drag);
+	div.addEventListener("dragover",allowDrop);
+	       div.addEventListener("drop", drop);
+});
+
+
+divs.forEach((div, index) => {
+    div.style.backgroundImage = `url('image${index + 1}.jpg')`;
+});
+
+
+
+
+
+
+
+
+
+
